43b6873ff4332a98e029f8729deb19706ef213d6
Michael Irby <michael.irbstoomuch@gmail.com>
2018-01-19 16:04:42 -0500

    tests update

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,3 +2,45 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    // Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts);
+    });
+});
+
+//Initial test on Mocha
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+//
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+//
+// let car;
+//
+// beforeEach(()=> {
+//     car = new Car();
+// });
+//
+// describe('Car', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped');
+//     });
+//
+//     it('can drive', ()=> {
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
